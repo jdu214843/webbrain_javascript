9daeb74efd9312976314d9eb545276e22171177c
jdu214843 <214843a@jdu.uz>
2023-02-24 17:11:39 +0500

    update js

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-let a; 
-let b = null;
 
-console.log(a == b);
+let a = 8;
+let b = 76;
+let c = 15;
+let n = a + b + c;
+
+if(a > b && b > c){
+    console.log(a);
+}else if(b > a && b > c){
+    console.log(b)
+}else{
+    console.log(c)
+}
+console.log(n);
