0fd46c6a7feaac01254c0fcd660c2862928bb2a8
jdu214843 <214843a@jdu.uz>
2023-02-24 21:44:03 +0500

    update

--- index.js
+++ index.js
@@ -1,14 +1,41 @@
 
-let a = 8;
-let b = 76;
-let c = 15;
-let n = a + b + c;
+// let a = 8;
+// let b = 76;
+// let c = 15;
+// let n = a + b + c;
 
-if(a > b && b > c){
-    console.log(a);
-}else if(b > a && b > c){
-    console.log(b)
-}else{
-    console.log(c)
+// if(a > b && b > c){
+//     console.log(a);
+// }else if(b > a && b > c){
+//     console.log(b)
+// }else{
+//     console.log(c)
+// }
+// console.log(n);
+
+
+let ar = ["Asilbek" , 19 , "Muhammadsaid"];
+let obj = {
+    name: "webbrain",
+    title: "IT Center"
 }
-console.log(n);
+
+// function test() {
+//     console.log(ar[1]);
+//     console.log(obj.title);
+// }
+
+// test()
+
+// let a = 4;
+// let b = '5';
+// console.log(++a + ++b);
+// console.log(a * b);
+// console.log(++a - b);
+
+ console.log(true == "1");
+
+
+
+
+
