c8de4faab5831fcb2efd0d53b342171eef22e4e9
jdu214843 <214843a@jdu.uz>
2023-02-25 11:30:54 +0500

    lesson 2

--- index.js
+++ index.js
@@ -14,11 +14,11 @@
 // console.log(n);
 
 
-let ar = ["Asilbek" , 19 , "Muhammadsaid"];
-let obj = {
-    name: "webbrain",
-    title: "IT Center"
-}
+// let ar = ["Asilbek" , 19 , "Muhammadsaid"];
+// let obj = {
+//     name: "webbrain",
+//     title: "IT Center"
+// }
 
 // function test() {
 //     console.log(ar[1]);
@@ -33,7 +33,123 @@ let obj = {
 // console.log(a * b);
 // console.log(++a - b);
 
- console.log(true == "1");
+//  console.log(true == "1");
+
+// let forma = true;
+// let soqol = true;
+// let pul = false;
+// console.log(forma && soqol && pul);
+
+// let a =  1;
+// let b = 1;
+
+// let c = ++a; // 2
+// let n = b++; // 2
+// console.log(c,b); //bu yerda a ni qiymati 1 oshib so'ng 2 o'tadi. b esa 1 chi 1 ni oladi keyin qiymatini birga oshiradi.
+// agar b ni ya'na qayta chaqirmasak u 1 qiymatni saqlab turadi.
+
+
+// let a = 2;
+
+// let b = 1 + (a *= 2); bu yerda a ni qiymatini 2 ko'paytirib keyin 1 qo'shadi. shunda consolega 5 chiqadi.
+// a ning qiymati 4 ga aylanadi agar a ni ham chaqirsak u 4 ga teng.
+
+// console.log(a, b);
+
+// console.log(2 ** (2 + 1) / 2);
+
+// let a = 2;
+// console.log(a ** 2)
+
+// console.log(null == undefined); // true chiqadi sababi ikkisi ham yo'qlikni bildiradi. ularning turini tekshirmaydi.
+// console.log(null === undefined) // bunda false chiqadi sababi ularni qiymatini ham typeof ni ham tekshiradi.
+                                // null typeof = object. undefined typeof = undefined.
+
+                        
+// let a = null;
+// console.log(a)
+// console.log(a + 1);
+// console.log(a--);
+// console.log(a--);
+
+
+// let a = 1;
+
+// console.log(true ==1);
+// console.log(true ===1);
+
+// console.log(true != false);
+// console.log(true !== false);
+
+// console.log(1 != 0);
+// console.log(1 !== "0");
+
+
+// ​2 - dars | Type Conversions | Operators | Comparison | Logical Operators | YouTube 
+// Link
+// —  Type convirsions
+
+// —  Operators + - * / % () = ++ -- **
+
+// —  Comparision == === <> <= >=
+
+// —  Logical Operations || && !
+
+// console.log(((5 + 2) + 2) / 2);
+
+// let a = 2;
+// let b = 4;
+// console.log(a)
+// console.log(a = b);
+
+// let a = 2;
+
+// console.log(a--);
+// console.log(a--);
+// console.log(--a);
+
+// let a = 5;
+// console.log(a ** 4);
+
+// let a = 5;
+// let b = 5;
+
+// console.log(a == b);
+
+// let a = 5;
+// let b = 6;
+// let c = 6;
+
+// console.log(a  >= b);
+// let a = 5;
+// let b = 5;
+// console.log(a != b);
+
+let a = 8;
+let b = 7;
+let c = 9;
+
+if(a > b && b > c){
+    console.log(a);
+}else if(b > a && a > c){
+    console.log(b);
+}else{
+    console.log(c);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
