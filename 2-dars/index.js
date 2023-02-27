781319e34fd9893429d540e1d0461110cb449e3f
jdu214843 <214843a@jdu.uz>
2023-02-27 18:03:28 +0500

    Type Conversions | Operators

---
+++ 2-dars/index.js
@@ -0,0 +1,51 @@
+// ​2 - dars | Type Conversions | Operators | Comparison | Logical Operators | YouTube 
+// Link
+// —  Type convirsions
+
+// —  Operators + - * / % () = ++ -- **
+
+// —  Comparision == === <> <= >=
+
+// —  Logical Operations || && !
+
+// console.log(((5 + 2) + 2) / 2);
+
+// let a = 2;
+// let b = 4;
+// console.log(a)
+// console.log(a = b);
+
+// let a = 2;
+
+// console.log(a--);
+// console.log(a--);
+// console.log(--a);
+
+// let a = 5;
+// console.log(a ** 4);
+
+// let a = 5;
+// let b = 5;
+
+// console.log(a == b);
+
+// let a = 5;
+// let b = 6;
+// let c = 6;
+
+// console.log(a  >= b);
+// let a = 5;
+// let b = 5;
+// console.log(a != b);
+
+// let a = 8;
+// let b = 7;
+// let c = 9;
+
+// if(a > b && b > c){
+//     console.log(a);
+// }else if(b > a && a > c){
+//     console.log(b);
+// }else{
+//     console.log(c);
+// }
