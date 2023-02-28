392807377f3f445c86194eb00274e497c1175d2e
jdu214843 <214843a@jdu.uz>
2023-02-28 16:04:58 +0500

    Switch statement and For Loops

---
+++ 4-dars/index.js
@@ -0,0 +1,30 @@
+// !! 4 - dars Switch statement and For Loops
+
+
+// if --> <> , <= >=, == ===
+// switch -> ===
+// let data = 'bulut'
+// switch(data){
+    // case 'yomgir':  
+//         console.log("bugun yomgir");
+//         console.log("bugun yomgir");
+//     break;
+
+//     case 'bulut':
+//         console.log("bugun qor");
+
+//         default: console.log("no data")
+// } 
+// !! case -  data aniq ma'lum bir qiymatga teng bo'lishini kutadi: 
+// !! case aniq qiymatga teng bo'lishi kerak:
+
+// ?? if dan farqi exicet: aniq ma'lumot qabul qiladi: va condition dek katta yoki kichik qabul qilmaydi:
+
+
+// ?? Foor Loops
+
+// !! for da 3 ta nuqta bor bular (boshlanish nuqtasi, manzil qayergacha, tugash nuqtasi)
+
+for( i = 0; i <= 10; i++){
+    console.log(i)
+};
