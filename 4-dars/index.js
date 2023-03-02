2be42016dff4f30c0f871240e7b130e17a7b1127
jdu214843 <214843a@jdu.uz>
2023-03-02 11:00:45 +0500

    dars Switch statement and For Loops

--- 4-dars/index.js
+++ 4-dars/index.js
@@ -25,6 +25,52 @@
 
 // !! for da 3 ta nuqta bor bular (boshlanish nuqtasi, manzil qayergacha, tugash nuqtasi)
 
-for( i = 0; i <= 10; i++){
-    console.log(i)
-};
+// for( i = 0; i <= 10; i++){
+//     console.log(i)
+// };
+
+
+
+// console.log(c);
+// let browser
+// switch(browser){
+//     case 'Edge':
+//         alert("You've got the Edge");
+//         break;
+
+//         case 'Chrome':
+//         case 'Firefox':
+//         case 'Safari':
+//         case 'Opera':
+//             alert('Okay we support these browsers too');
+//             break;
+//         default:
+//             alert('We hope that this page looks ok!');
+// }
+
+// let a = +prompt('a?' , '');
+
+// if(a == 0){
+//     alert( 0 )
+// }
+// if(a == 1){
+//     alert( 1 )
+// }
+// if(a == 2 || a == 3){
+//     alert('2,3');
+// }
+
+
+// let a = 35235;
+// let b = 0;
+// let c = 0;
+// while(a > 0){
+//     c = c + (a % 10);
+//     b = a % 10;
+//     console.log(b);
+
+//     a = (a - b) / 10;
+//     console.log(a);
+
+//     b = 0;
+// }
