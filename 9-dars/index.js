7abf1ae9a030700510c2229e97ccaf1809d336fe
jdu214843 <214843a@jdu.uz>
2023-03-16 09:58:23 +0500

    String

---
+++ 9-dars/index.js
@@ -0,0 +1,54 @@
+// !! String Homework
+// masala
+// function Count(num){
+//     let binary = num.toString(2)
+//     let a = 0
+//     let b = 0
+//     for(let i = 0; i<binary.length; i++){
+//         if((binary[i] - 0)===1){
+//             a++;
+//         }else{
+//             b++;
+//         }
+//     }
+//     return `${a} ta raqam ${b} ta nol`
+// }
+// console.log(Count(50));
+
+
+// ?? 1 -masala
+
+// let str1 = "webBrain academy"
+// let str2 = "webBrain academy"
+// check = (a,b) =>{
+//     console.log(a.localeCompare(b))
+// }
+// check(str1, str2)
+
+// !! 2-masala
+
+// let str = "webbrain"
+
+// getCount = (a) =>{
+//     console.log(a.slice());
+// }
+// getCount(str)
+
+// let str = "webbrain"
+// function getCount(a){
+//     console.log(a.split(""));
+    
+// }
+// getCount(str)
+
+// ?? 3-masala
+
+// let str = "webbrain"
+
+// function turncate(cat,n){
+//     console.log(cat.slice(0, n));
+// }
+// turncate(str,5)
+
+// ?? 4-masala
+
