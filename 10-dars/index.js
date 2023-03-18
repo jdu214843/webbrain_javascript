7ddec3940a7c84cb1c706ad52bebc8cb0bfc7b42
jdu214843 <214843a@jdu.uz>
2023-03-18 22:03:14 +0500

    Arreys

--- 10-dars/index.js
+++ 10-dars/index.js
@@ -1,2 +1,19 @@
 // !! Arrays
 
+const paragraph = (str) => {
+        let res = ""
+        let count = 1
+        for (let i = 0; i < str.length; i++) {
+            if (str[i] === str[i + 1]) {
+                count++
+            } else {
+                res += str[i]
+                if (count > 1) {
+                    res += count
+                    count = 1
+                }
+            }
+        }
+        console.log(res)
+    }
+paragraph("webbbrainnn")
