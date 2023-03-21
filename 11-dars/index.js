154baa5419bae6c5105d2ab7ff036cdeb911162e
jdu214843 <214843a@jdu.uz>
2023-03-21 21:42:53 +0500

    Arrays

---
+++ 11-dars/index.js
@@ -0,0 +1,28 @@
+// !! Arrays 2 qism
+
+// Advanced arrays methods
+
+// let num  = [1,3,4,5,6,0];
+// let str = ['orange', 'apple', 'banana', "kivi"];
+
+// let obj = [
+//     {id: 1, year : 2003, name:"asilbek"},
+//     {id: 2, year : 2002, name:"jasur"},
+//     {id: 3, year : 1995, name:"javohir"},
+//     {id: 4, year : 2002, name:"sardor"},
+//     {id: 5, year : 2003, name:"saidasror"},
+//     {id: 6, year : 2002, name:"Muhammadsiddiq"},
+//     {id: 7, year : 2002, name:"Muhammadsodiq"},
+//     {id: 8, year : 2002, name:"Muhammadsaid"},
+// ];
+
+// let res  = obj.filter((value)=>value.id >= 5)
+// let res = str.filter(function(value, index){
+//     return value.length <= 5
+// });
+// let res = num.filter((value,index)=>{
+//     return  value < 5
+// });
+// console.log(res)
+
+
