938cbec32277c21ef5c036311b7e5b364f246b11
jdu214843 <214843a@jdu.uz>
2023-03-23 13:05:53 +0500

    Strings

--- 10-dars/index.js
+++ 10-dars/index.js
@@ -1,25 +1,4 @@
 // !! Arrays
 
 
-// 1 - masala
-console.log(eval([1,2,3,4].join("+")))
 
-// 2-masala
-
-// const paragraph = (str) => {
-//         let res = ""
-//         let count = 1
-//         for (let i = 0; i < str.length; i++) {
-//             if (str[i] === str[i + 1]) {
-//                 count++
-//             } else {
-//                 res += str[i]
-//                 if (count > 1) {
-//                     res += count
-//                     count = 1
-//                 }
-//             }
-//         }
-//         console.log(res)
-//     }
-// paragraph("webbbrainnn")
--- 9-dars/index.js
+++ 9-dars/index.js
@@ -18,10 +18,12 @@
 
 // ?? 1 -masala
 
+
+
 // let str1 = "webbrain academy"
-// let str2 = ""
+// let str2 = "feafa"
 // let res = str1.localeCompare(str2)
-// function check (res){
+
 //     if(res < 0){
 //         console.log("mutlaqo teng emas")
 //     }else if(res > 0){
@@ -29,32 +31,8 @@
 //     }else{
 //         console.log("ikkisi teng")
 //     }
-// }
-// check(res)
 
-let str1 = "webbrain academy"
-let str2 = "feafa"
-let res = str1.localeCompare(str2)
 
-    if(res < 0){
-        console.log("mutlaqo teng emas")
-    }else if(res > 0){
-        console.log("qisman o'xshash")
-    }else{
-        console.log("ikkisi teng")
-    }
-
-var oStringObject1 = new String("brick");
-var oStringObject2 = new String("");
-var iResult = oStringObject1.localeCompare(oStringObject2);
-
-if(iResult < 0) {
-    console.log("teng emas");
-} else if (iResult > 0) {
-    console.log("qisman");
-} else {
-    console.log("The two strings are equal");
-}
 
 // !! 2-masala
 
@@ -83,3 +61,75 @@ if(iResult < 0) {
 
 // ?? 4-masala
 
+// function getData(str){
+//     let res = {num:0, letter: 0, char: 0};
+//     for(i of str){
+//         if(isFinite(i)){
+//             res.num++
+//         }else if(i.toLowerCase() !== i.toUpperCase()){
+//             res.letter++
+//         }else{
+//             res.char++
+//         }
+//     }
+//     console.log(res)
+// }
+
+// getData("webbr@#$%n2022")
+
+// function getUpperCase(str){
+//     res = "";
+//     for(i in str){
+//         res += str[i - 1] === " " || str[i - 1] === undefined ? 
+//         str[i].toUpperCase(): str[i]
+//     }
+//     console.log(res)
+// }
+// getUpperCase("webbrain academy")
+
+// 1 - masala
+// console.log(eval([1,2,3,4].join("+")))
+
+// 2-masala
+
+// const paragraph = (str) => {
+//         let res = ""
+//         let count = 1
+//         for (let i = 0; i < str.length; i++) {
+//             if (str[i] === str[i + 1]) {
+//                 count++
+//             } else {
+//                 res += str[i]
+//                 if (count > 1) {
+//                     res += count
+//                     count = 1
+//                 }
+//             }
+//         }
+//         console.log(res)
+//     }
+// paragraph("webbbrainnnnnn")
+
+// let res = "";
+// function getDubl(str){
+//     for(i of str){
+//         res += i
+//      }
+//     console.log(res)
+// }
+
+// getDubl("webbrain")
+
+
+// let res = "10010001011111001"
+// let count = 0;
+// let s = 0;
+// for(i of res){
+//     if(i == 1){
+//         count++
+//     }else{
+//         count > s && (s = count)
+//         count = 0
+//     }
+// }
+// console.log(s)
