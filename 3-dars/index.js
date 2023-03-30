21662fe61623d8a275101335570b17dafa6c5c91
jdu214843 <214843a@jdu.uz>
2023-03-30 10:11:15 +0500

    if and else

--- 3-dars/index.js
+++ 3-dars/index.js
@@ -30,12 +30,11 @@
 // }
 // console.log(result)
 
-
 // ?? 4-masala
 
 // let login = "Director";
 // let message;
- 
+
 // login == "Employee" ? message = "Hello"
 // : (login == "Director")
 // ? message = "Greetings"
@@ -45,6 +44,75 @@
 
 // console.log(message);
 
+// let x = 7;
+// let y = -3;
 
+// const getDekart = (a, b) => {
+//   if (a > 0 && b > 0) {
+//     console.log("1-chorak");
+//   } else if (a < 0 && b > 0) {
+//     console.log("2-chorak");
+//   } else if (a < 0 && b < 0) {
+//     console.log("3-chorak");
+//   } else {
+//     console.log("4-chorak");
+//   }
+// };
+// getDekart(x, y);
 
+// let a = 1;
+// let b = 4;
+// let c = 2;
 
+// const getFunction = (x, y, z) => {
+//   if (x + y > z && x + z > y && y + z > x) {
+//     console.log("yes");
+//   } else {
+//     console.log("No");
+//   }
+// };
+
+// getFunction(a, b, c);
+
+// let x = 12;
+
+// const getFunction = (a) => {
+//   if (a <= 0) return -a;
+//   if (a > 0 && a < 2) return a * a;
+//   if (a >= 2) return 4;
+// };
+
+// console.log(getFunction(x));
+
+// let a = 10;
+
+// const getType = (x) => {
+//   if (x == 0) return "nol";
+//   if (x > 0) {
+//     if (x % 2 == 0) {
+//       return "musbat juft";
+//     } else {
+//       return "musbat toq";
+//     }
+//   } else {
+//     if (x % 2 == 0) {
+//       return "manfiy juft";
+//     } else {
+//       return "manfiy toq";
+//     }
+//   }
+// };
+// console.log(getType(a));
+
+// let a = 4875;
+
+// const getSon = (x) => {
+//   let b;
+//   if (x % 2 == 0) {
+//     b = " xonali juft son";
+//   } else {
+//     b = " xonali toq son";
+//   }
+//   return String(String(x).length).concat(b);
+// };
+// console.log(getSon(a));
