50f7efdc5ed11b648903e6e4f91dea96a0a50044
jdu214843 <214843a@jdu.uz>
2023-04-06 18:02:07 +0500

    recursion Function

--- 14-dars/index.js
+++ 14-dars/index.js
@@ -1,15 +1,48 @@
-function sum(a) {
-  if (a === undefined) {
-    return 0;
-  }
+// function sum(a) {
+//   if (a === undefined) {
+//     return 0;
+//   }
 
-  return function innerSum(b) {
-    if (b === undefined) {
-      return a;
+//   return function innerSum(b) {
+//     if (b === undefined) {
+//       return a;
+//     }
+//     return sum(a + b);
+//   };
+// }
+
+// const result = sum(5)(4)(3)(6)(7)(5)(8)();
+// console.log(result);
+
+// let list = [
+//   { id: 1, name: "eshmat", title: "CEO" },
+//   { id: 2, name: "toshmat", title: "CTO" },
+//   { id: 3, name: "eshmat", title: "CEO" },
+//   { id: 4, name: "toshmat", title: "CTO" },
+// ];
+
+// let res = {};
+// let ceo = [];
+// let cto = [];
+
+// let result = list.map((value) => {});
+
+let list = [
+  { id: 1, name: "eshmat", title: "CEO" },
+  { id: 2, name: "toshmat", title: "CTO" },
+  { id: 3, name: "eshmat", title: "CTO" },
+  { id: 4, name: "toshmat", title: "SES" },
+  { id: 5, name: "toshmat", title: "SES" },
+];
+
+function getGroup(data) {
+  let res = {};
+  data.filter((value) => {
+    if (!res[value.title]) {
+      res[value.title] = [];
     }
-    return sum(a + b);
-  };
+    res[value.title].push(value);
+  });
+  return res;
 }
-
-const result = sum(5)(4)(3)(6)();
-console.log(result);
+console.log(getGroup(list));
