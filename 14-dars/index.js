753a72351d7efde8e0c8b2f6af714eb1a8e6aae0
jdu214843 <214843a@jdu.uz>
2023-04-11 15:34:31 +0500

    Weakmap new set, newmap , Weakmap

--- 14-dars/index.js
+++ 14-dars/index.js
@@ -27,21 +27,50 @@
 
 // let result = list.map((value) => {});
 
-let list = [
-  { id: 1, name: "eshmat", title: "CEO" },
-  { id: 1, name: "eshmat", title: "CEO" },
-  { id: 2, name: "toshmat", title: "CTO" },
-  { id: 4, name: "toshmat", title: "SES" },
-  { id: 3, name: "eshmat", title: "CTO" },
-  { id: 5, name: "toshmat", title: "SES" },
-];
+// let list = [
+//   { id: 1, name: "eshmat", title: "CEO" },
+//   { id: 1, name: "eshmat", title: "CEO" },
+//   { id: 2, name: "toshmat", title: "CTO" },
+//   { id: 4, name: "toshmat", title: "SES" },
+//   { id: 3, name: "eshmat", title: "CTO" },
+//   { id: 5, name: "toshmat", title: "SES" },
+// ];
 
-const getTitle = (data) => {
-  let res = [];
-  data.map((value) => {
-    res[value.title] = new Array();
-    res[value.title].push(value);
-  });
-  return res;
-};
-console.log(getTitle(list));
+// const getTitle = (data) => {
+//   let res = [];
+//   data.map((value) => {
+//     res[value.title] = new Array();
+//     res[value.title].push(value);
+//   });
+//   return res;
+// };
+// console.log(getTitle(list));
+
+// const myWeakSet = new WeakSet();
+// const obj1 = {};
+// myWeakSet.add(obj1);
+// console.log(obj1);
+
+// const myWeakSet = new WeakSet();
+// const obj1 = {};
+// myWeakSet.add(obj1);
+// console.log(myWeakSet.has(obj1)); // true
+
+// const myWeakSet = new WeakSet();
+// const obj1 = {};
+// myWeakSet.add(obj1);
+// console.log(myWeakSet.has(obj1)); // true
+// myWeakSet.delete(obj1);
+// console.log(myWeakSet.has(obj1)); // false
+
+// WeakMap
+
+// const myWeakMap = new WeakMap();
+// const obj1 = {};
+// myWeakMap.set(obj1, "Extra data for Object 1");
+// console.log(myWeakMap.get(obj1)); // Extra data for Object 1
+
+// const myWeakMap = new WeakMap();
+// const obj1 = {};
+// myWeakMap.set(obj1, "Extra data for Object 1");
+// console.log(myWeakMap.get(obj1)); // "Extra data for Object 1"
