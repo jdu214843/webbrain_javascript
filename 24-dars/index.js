a6d77d71c4743cad8c413ee933fcf8e528394a2d
jdu214843 <214843a@jdu.uz>
2023-04-14 15:55:45 +0500

    Homework

--- 24-dars/index.js
+++ 24-dars/index.js
@@ -1,12 +1,14 @@
 // Classes Advanced Methods
 
-let a = 124;
+let a = 12345678;
 let c = 0;
 let b = 0;
 
 while (a > 0) {
   c = c + (a % 10);
+  //   console.log(a);
   b = a % 10;
+  //   console.log(b);
   a = (a - b) / 10;
 }
 console.log(c);
