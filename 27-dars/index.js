ac61f7ece378f468915da49bc0f884566f26317e
jdu214843 <214843a@jdu.uz>
2023-04-25 12:47:17 +0500

    Proxy

--- 27-dars/index.js
+++ 27-dars/index.js
@@ -1,14 +1,24 @@
 // ?? proxy proxy mavzusi
 let user = {
   name: "asilbek",
-  pw: 12434,
+  _pw: 12434,
 };
-let proxy = new Proxy(user, {});
+// let proxy = new Proxy(user, {
+//   ownKeys(target) {
+//     return Object.keys(target).filter((value) => value !== "_pw");
+//   },
+// });
 
-const getProxy = (user) => {
-  get(target);
-  return Object.keys(target).filter((value) => value !== "pw");
+// for (value in proxy) {
+//   console.log(value);
+// }
+
+let range = {
+  from: 1,
+  to: 10,
 };
-
-console.log(proxy);
-console.log(user);
+let proxy = new Proxy(range, {
+  has(target, prop) {
+    return target.from >= prop && target.to <= prop;
+  },
+});
